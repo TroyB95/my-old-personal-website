5361ae8f8d6fab456d4acb833dcc8af9e4cd6b48
TroyB95 <Troy_budd95@msn.com>
2018-11-06 09:26:59 +0000

    Built static site, test for mobile.

---
+++ gatsby-portfolio/component---src-pages-404-js-0b70a8614b31efc6ed54.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{134:function(n,e,t){"use strict";t.r(e);var a=t(0),u=t.n(a);e.default=function(){return u.a.createElement("div",null,u.a.createElement("h4",null,"Page not found"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-0b70a8614b31efc6ed54.js.map
